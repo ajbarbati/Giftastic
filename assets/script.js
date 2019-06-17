204e3174a42e42ebe0b1e1f1de9ec876489912f2
Alex B <ajbarbati@gmail.com>
2019-06-17 17:48:59 -0400

    more js bugs

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,29 @@
+// jquery stuff > 
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9"
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
+  
 // array of what buttons will be avalible
-const topics = ['Moon', 'Star', 'Sun', 'Nebula', 'Earth', 'Mars', 'Blackhole', 'Dark Matter', 'Meteorite', 'Galaxy',]
+var topics = ['Moon', 'Star', 'Sun', 'Nebula', 'Earth', 'Mars', 'Blackhole', 'Dark Matter', 'Meteorite', 'Galaxy',]
  
 //function that appends all the buttons to the page
 function buttonMaker () {
-    for (let i = 0; i > topicsArray.length; i++) {
-        $("#whereTheBtnsGo").append("<button value='Moon'>" + topics[i] + "</button")
-        console.log("farts" + i)
+    for (var i = 0; i < topics.length; i++) {
+       console.log("farts1") 
+
+       var btn = $('<button>')
+
+       btn.addClass('buttons')
+       btn.attr('data-name' + topics[i])
+       btn.text(topics[i])
+        
+       $("#whereTheBtnsGo").append(btn)
     }
 }
 
+buttonMaker()

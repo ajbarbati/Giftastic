8ce3fa82ff6bb280b710a7c8b73a943c14f876ea
Alex B <ajbarbati@gmail.com>
2019-06-17 19:03:07 -0400

    experimenting trying to get gifs to appear on page

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,44 @@
 // jquery stuff > 
-var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9"
+// var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9"
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-  });
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   }).then(function(response) {
+//     console.log(response);
+//   });
 
 // array of what buttons will be avalible
 var topics = ['Moon', 'Star', 'Sun', 'Nebula', 'Earth', 'Mars', 'Blackhole', 'Dark Matter', 'Meteorite', 'Galaxy',]
  
 //function that appends all the buttons to the page
 function buttonMaker () {
+    
     for (var i = 0; i < topics.length; i++) {
         
-            var $btn = $('<input type="button" value="' + topics[i] + '" />');
-            $btn.appendTo($("body"));
-    
-    //    var btn = $('<button>')
-
-    //    btn.addClass('buttons')
-    //    btn.attr('data-name' + topics[i])
-    //    btn.text(topics[i])
-        
-    //    $("#whereTheBtnsGo").append(btn)
-       console.log("farts" + topics[i])
+            var $btn = $('<input type="button" value="' + topics[i] + '" />').attr({'data-name': topics[i]})
+            $btn.appendTo($('#btns'))
     }
+
+    $($btn).on('click', function(){
+        
+        $('#gifs').empty()
+
+		var thisTopic = $(this).attr('data-name')
+        
+        var giphyURL = "http://api.giphy.com/v1/gifs/search?q=" + thisTopic + "&limit=10&api_key=anyHsKhH5D2riRQBMncMV5DVInUAtbnc";
+        console.log('farts')
+		$.ajax({
+            url: giphyURL, 
+            method: 'GET'
+        }).done( function (giphy) {
+            console.log('123')
+			currentGif = giphy.data;
+			$.each(currentGif, function (value){
+                console.log('456')
+			});
+		});
+	});
 }
 
 buttonMaker()
